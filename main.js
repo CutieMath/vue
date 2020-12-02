384c9cd9d975093cf552b3900bfd884abd411518
CutieMath <cutie.math@protonmail.com>
2020-12-02 22:59:52 +1100

    two components

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+Vue.component('task-list', {
+    template: '<div><task v-for="task in tasks">{{ task.description }}</task></div>',
+    data(){
+        return {
+            tasks: [
+                { description: 'Being cute', complete: true},
+                { description: 'Being sweet', complete: false},
+                { description: 'Being honey', complete: false},
+                { description: 'Being yummy', complete: true},
+            ]
+        };
+    }
+});
+
 Vue.component('task', {
-    template: '<p><slot></slot></p>',
+    template: '<li><slot></slot></li>'
 });
 
 new Vue({
     el: '#root'
-})
+});
