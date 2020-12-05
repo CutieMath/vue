ace513637cd07a8f9c5775099086c825baf78890
CutieMath <cutie.math@protonmail.com>
2020-12-05 19:50:27 +1100

    modal component

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
+// message component
 Vue.component('baby-message', {
     props: ['title', 'body'],
-
     data() {
         return {
             isVisible: true
         };
     },
-
     template: `
     <div>
         <article class="message m-5" v-show="isVisible">
@@ -20,7 +19,6 @@ Vue.component('baby-message', {
         </article>
     </div>
   `,
-
   methods: {
       hideModal(){
           this.isVisible = false;
@@ -28,6 +26,25 @@ Vue.component('baby-message', {
   }
 });
 
+
+// modal component 
+Vue.component('baby-modal', {
+    template: `
+    <div class="modal is-active">
+        <div @click="$emit('close')" class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">
+                <slot></slot>
+            </div>
+        </div>
+        <button @click="$emit('close')" class="modal-close is-large" aria-label="close"></button>
+    </div>
+    `
+})
+
 new Vue({
-    el: '#root'
+    el: '#root', 
+    data: {
+        showModal: false
+    }
 });
