d1b802f1d697336bc2bc7077a02bb40b83553693
CutieMath <cutie.math@protonmail.com>
2020-12-05 20:36:29 +1100

    tabs

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ Vue.component('baby-message', {
 // modal component 
 Vue.component('baby-modal', {
     template: `
-    <div class="modal is-active">
+    <div class="modal is-active is-centered">
         <div @click="$emit('close')" class="modal-background"></div>
         <div class="modal-content">
             <div class="box">
@@ -40,6 +40,61 @@ Vue.component('baby-modal', {
         <button @click="$emit('close')" class="modal-close is-large" aria-label="close"></button>
     </div>
     `
+})
+
+
+// tabs component
+Vue.component('tabs', {
+    template: `
+    <div>
+        <div class="tabs is-centered">
+            <ul>
+                <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }"> 
+                    <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+                </li>
+            </ul>
+        </div>
+        <div class="tabs-details">
+            <slot></slot>
+        </div>
+    </div>
+    `,
+    data(){
+        return { tabs: [] };
+    },
+    created(){
+        this.tabs = this.$children;
+    }, 
+    methods: {
+        selectTab(selectedTab){
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name == selectedTab.name);
+            });
+        }
+    }
+})
+
+Vue.component('tab', {
+    props: {
+        name: { required: true },
+        selected: { default: false }
+    },
+    template: `
+        <div v-show="isActive"><slot></slot></div>
+    `,
+    data() {
+        return {
+            isActive: false
+        }
+    },
+    computed: {
+        href(){
+            return '#' + this.name.toLowerCase().replace(/ /g, '-' );
+        }
+    },
+    mounted() {
+        this.isActive = this.selected;
+    }
 })
 
 new Vue({
