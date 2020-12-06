0ebc73888870f2e1a696215c664206e4c20c3f1a
CutieMath <cutie.math@protonmail.com>
2020-12-06 18:45:58 +1100

    add bootstrapVue and style Header

--- vue02/src/App.vue
+++ vue02/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <QuestionBox />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuestionBox from './components/QuestionBox.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QuestionBox,
+    Header
   }
 }
 </script>
---
+++ vue02/src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <b-nav tabs align="center">
+            <b-nav-item disabled><b>Cutie App</b></b-nav-item>
+            <b-nav-item disabled>Counter: 4/10</b-nav-item>
+        </b-nav>
+    </div>
+</template>
