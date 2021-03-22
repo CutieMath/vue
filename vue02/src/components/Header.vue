1054552f4f31118fa6991bc744df2a43e13a0fbc
CutieMath <cutie.math@protonmail.com>
2021-03-22 22:18:23 +1100

    started again

--- vue02/src/components/Header.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-        <b-nav tabs align="center">
-            <b-nav-item disabled><b>Cutie Quiz App</b></b-nav-item>
-            <b-nav-item disabled>Counter: 4/10</b-nav-item>
-        </b-nav>
-    </div>
-</template>
