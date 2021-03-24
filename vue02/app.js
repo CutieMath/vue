6b8764b6444bc1e5fadd5a7eefa5c6731b679809
CutieMath <cutie.math@protonmail.com>
2021-03-24 23:04:09 +1100

    use methods in mustush

--- vue02/app.js
+++ vue02/app.js
@@ -5,6 +5,17 @@ const app = Vue.createApp({
             vueLink: 'https://www.udemy.com/course/vuejs-2-the-complete-guide/learn/lecture/21463138#questions/12725120',
         };
     },
+    // functions
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5 ) {
+                return 'Solve Proto questions at hand.';
+            } else {
+                return 'Feel comfortable to do my own Vue projects.';
+            }
+        }
+    }
 });
 
 app.mount('#user-goal');
