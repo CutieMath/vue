26fda3f6061057fc862f6d809b442573336dfcde
CutieMath <cutie.math@protonmail.com>
2021-03-24 23:48:56 +1100

    finish first assignment

---
+++ vue02-assignment/app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            cutieName: 'Yuxin',
+            cutieAge: '30',
+            cutieAgeInFive: '35',
+            imageLink: 'https://cdn130.picsart.com/289106460014201.jpg?type=webp&to=min&r=640',
+        }
+    },
+    methods: {
+
+    }
+});
+
+app.mount('#assignment')
