648d20acd058b33efec2f5ae36c04272fcfc2afb
CutieMath <cutie.math@protonmail.com>
2021-03-26 23:43:55 +1100

    finish assignment 😎

---
+++ vue03-assignment2/app.js
@@ -0,0 +1,39 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            number: 0,
+        }
+    },
+    computed: {
+        result() {
+            if (this.number < 37) {
+                return "Not there yet x Do it again"
+            } else if (this.number > 37) {
+                return "TOO MUCH!";
+            } else {
+                return this.number;
+            }
+        }
+    },
+    watch: {
+        number(value){
+            if (value > 37) {
+                const that = this;
+                setTimeout(function (){
+                    that.number = 0;
+                }, 5000);
+            }
+        }
+    },
+    methods: {
+        addFive() {
+            this.number += 5;
+        },
+        addOne() {
+            this.number += 1;
+        }
+    }
+
+});
+
+app.mount("#assignment");
