ab16362b69f6b1e5241c7300aa3a8e1611a2de95
CutieMath <cutie.math@protonmail.com>
2021-04-05 19:26:13 +1000

    add in health bar

---
+++ vue06/app.js
@@ -0,0 +1,28 @@
+// outsource the function outside of Vue
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// vue
+const app = Vue.createApp({
+    data(){
+        return {
+            playerHealth: 100,
+            monsterHealth: 100
+        };
+    },
+    methods: {
+        attackMonster(){
+            // fomular to get random value between 5 to 12
+            const monsterDamage = getRandomValue(5, 15);
+            this.monsterHealth -= monsterDamage;
+            this.attackPlayer();
+        },
+        attackPlayer(){
+            const playerDamage = getRandomValue(5, 15);
+            this.playerHealth -= playerDamage;
+        }
+    }
+});
+
+app.mount("#game");
