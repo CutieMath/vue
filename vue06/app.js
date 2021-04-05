e2c604df36337f6e853596f5dc9bb5ba7140806a
CutieMath <cutie.math@protonmail.com>
2021-04-05 20:30:28 +1000

    add surrender and show buttons

--- vue06/app.js
+++ vue06/app.js
@@ -79,6 +79,9 @@ const app = Vue.createApp({
             this.playerHealth = 100;
             this.monsterHealth = 100;
             this.winner = null;
+        },
+        surrender(){
+            this.winner = 'monster';
         }
     }
 });
