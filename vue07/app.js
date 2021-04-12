31c8d4b2df8accbaeef250f2d82a32d2264a14b0
CutieMath <cutie.math@protonmail.com>
2021-04-12 22:55:29 +1000

    finish friendlist first edition

---
+++ vue07/app.js
@@ -0,0 +1,37 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            detailsVisible: true,
+            friends: [
+                {
+                    id: 'Cutie', 
+                    name: 'Cutie Math', 
+                    phone: '123-456789', 
+                    email: 'cutie@protonmail.com' 
+                },
+                {
+                    id: 'Baby', 
+                    name: 'Baby G', 
+                    phone: '000-456555', 
+                    email: 'baby@protonmail.com' 
+                },
+            ]
+        }
+    },
+    computed: {
+        showDetailStatus() {
+            if (this.detailsVisible) {
+                return 'Hide Details';
+            } else {
+                return 'Show Details';
+            }
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsVisible = !this.detailsVisible;
+        }
+    }
+});
+
+app.mount('#app');
