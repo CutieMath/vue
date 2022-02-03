ec7f0be81395519dcb5297aefd783209674a7de0
CutieMath <cutie.math@protonmail.com>
2022-02-03 21:25:40 +1100

    use Vue to replace vanilla

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,13 +1,27 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+    data () {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal(){
+            this.goals.push(this.enteredValue);
+            this.enteredValue = "";
+        }
+    }
+}).mount('#app');
 
-function add2022Goal(){
-    const enteredValue = inputEl.value;
-    const listItemEl = document.createElement('li');
-    listItemEl.textContent = enteredValue;
-    listEl.appendChild(listItemEl);
-    inputEl.value = "";
-}
-
-buttonEl.addEventListener('click', add2022Goal);
+// Vanilla
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+// function add2022Goal(){
+//     const enteredValue = inputEl.value;
+//     const listItemEl = document.createElement('li');
+//     listItemEl.textContent = enteredValue;
+//     listEl.appendChild(listItemEl);
+//     inputEl.value = "";
+// }
+// buttonEl.addEventListener('click', add2022Goal);
