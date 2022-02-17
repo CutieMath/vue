991e08dc0dcc463433ca8d8c11f3aba6f575fe3c
CutieMath <cutie.math@protonmail.com>
2022-02-17 19:26:20 +1100

    use this for data created inside vue app

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            myGoal: "Master Vue and make projects with Vue.",
+            myGoalA: "Master Vue.",
+            myGoalB: "Make APPs use Vue.",
             deepWorkURL: "https://www.youtube.com/watch?v=2ybOJMibAbA&ab_channel=TheArtofImprovement"
         };
     },
@@ -9,9 +10,9 @@ const app = Vue.createApp({
         randomiseGoal() {
             const randomNumber = Math.random();
             if (randomNumber < 0.5) {
-                return "Master Vue";
+                return this.myGoalA;
             } else {
-                return "Implement Deep Work";
+                return this.myGoalB;
             }
         }
     }
