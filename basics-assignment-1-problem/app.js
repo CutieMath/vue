c5fe22e48eb3ec78fbcd3a3263c06a199b5b9e8c
CutieMath <cutie.math@protonmail.com>
2022-02-22 21:54:34 +1100

    finish assignment1

--- basics-assignment-1-problem/app.js
+++ basics-assignment-1-problem/app.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data(){
         return {
             myName: "Cutie",
-            myAge: 30
+            myAge: 30,
+            imageURL: "https://cdn.gizmostory.com/wp-content/uploads/2022/01/My-Dress-Up-Darling-Episode-1-Anime-Corner-2.jpg?strip=all&lossy=1&ssl=1",
+            textValue: "Cutie Vue x"
         }
     },
     methods: {
